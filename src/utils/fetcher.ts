96fcfa92ac37f72e29e405fbeab32fc68be94489
francisamidu <francisamidu124@gmail.com>
2023-03-16 16:57:42 +0200

    added fetcher wrapper and OPENAI types

---
+++ src/utils/fetcher.ts
@@ -0,0 +1,10 @@
+function fetcher<TResponse>(
+    url: string,
+    config: RequestInit = {}
+  ): Promise<TResponse> {
+    return fetch(url, config)
+      .then((response) => response.json())
+      .then((data) => data as TResponse);
+  }
+  export default fetcher;
+  
