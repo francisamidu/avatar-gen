794d6de1a48987e8d7fc9eef8d42185a067866a7
francisamidu <francisamidu124@gmail.com>
2023-03-17 12:11:25 +0200

    update button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,7 +10,7 @@ const MainButton = ({
   onClick,
 }: ButtonProps) => {
   const style = `text-white bg-green-700 hover:bg-green-800 font-medium rounded text-sm px-5 py-2.5 text-center inline-flex items-center outline-none ${
-    loading ? 'cursor-not-allowed hover:bg-green-700' : ''
+    loading ? 'cursor-not-allowed hover:!bg-green-700' : ''
   }`;
   const styles = classNames ? `${style} ${classNames}` : style;
   const handleClick = () => {
