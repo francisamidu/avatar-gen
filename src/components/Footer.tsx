fd67653bbc3aa17b079dc7c9ce1bbdbb2586f69b
francisamidu <francisamidu124@gmail.com>
2023-03-17 08:52:14 +0200

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,8 @@ import { Twitter, Github } from 'lucide-react';
 
 const Footer = () => {
   return (
-    <footer className="w-full bg-white">
-      <div className="flex max-w-screen-lg  flex-row items-center justify-between border-t border-t-gray-200 py-4 md:mx-auto">
+    <footer className="w-full text-slate-300">
+      <div className="flex max-w-screen-lg  flex-row items-center justify-center py-4 md:mx-auto">
         <p>
           Made by{' '}
           <a
@@ -15,17 +15,6 @@ const Footer = () => {
             Francis Amidu
           </a>
         </p>
-        <div className="flex flex-row items-center">
-          <a href="https://twitter.com/iamfrancisamidu" className="font-bold">
-            <Twitter className="mr-2" size={25} />
-          </a>
-          <a
-            href="https://github.com/francisamidu/avatar-gen"
-            className="font-bold"
-          >
-            <Github className="ml-2" size={25} />
-          </a>
-        </div>
       </div>
     </footer>
   );
